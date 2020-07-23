63baf490cde1d8a16a5cc87b521732185c876e1b
cjc-tawa <67713729+cjc-tawa@users.noreply.github.com>
2020-07-24 10:59:10 +1200

    in class changes
    nothing much getting ready for proper query

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="en-NZ">
 <head> 
 	<!-- NOTES: 
-		- Link to paste in browser: localhost/teacher/index.php
+		- Link to paste in browser: localhost/CSI_WEB_BUILD/index.php
 	-->
 	<title>PHP Build</title>
 	<meta charset="utf-8">
 	<meta name="author" content="Corey J Christensen">
 	<meta name="description" content="Building a website using HTML, CSS, and PHP.">
-	<meta name="keywords" content="HTML, CSS, PHP, XAMPP, MySQL, website build, NCEA, databases">
+	<meta name="keywords" content="HTML, CSS, PHP, XAMPP, MySQL, website build, databases">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Fonts imported using Google Fonts. -->
@@ -24,13 +24,12 @@
         require_once("connect.php");
         ?>
         <nav id="navbar">
-        <div id="bold"><center><h1>Music Database</h1></center></div>
+        <div id="bold"><h1>Music Database</h1></div>
                 <nav id='links'>
                         <ul>
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href="query2.php">Other query</a></li>
-                                <!--<li><a href="query3.php">Clothing</a></li>-->
-                                <!--<li><a href="query4.php">Not Clothing</a></li>-->
+                                <li><a href="index.php">Albums</a></li>
+                                <li><a href="query2.php">Artists</a></li>
+                                <li><a href="query3.php">Songs</a></li>
                         </ul>
                 </nav>
         </nav>
@@ -48,11 +47,11 @@
                                         {
                                 ?>
                                 
-                                        <div class="headings2">
+                                        
                                                 
-                                                <div><p><?php echo $output['album'];?><br>$<?php echo $output['album']; ?><br><?php echo $output['album']; ?><h3><?php echo $output['title']; ?></h3></p></div>
+                                        <div class="query_items"><p><?php echo $output['album'];?><?php echo $output['album']; ?><?php echo $output['album']; ?><?php echo $output['title']; ?></p></div>
                                                 
-                                        </div>
+                                        
                                         <?php
                                         } 
                                   ?>
