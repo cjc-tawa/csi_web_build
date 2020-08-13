ebe9cd5e5204ee9b57daa1b5a857f5c2ad5bb91c
cjc-tawa <67713729+cjc-tawa@users.noreply.github.com>
2020-08-13 12:21:05 +1200

    Changes to get ready for lockdown
    im lazy

--- index.php
+++ index.php
@@ -7,20 +7,40 @@
     <link rel="stylesheet" href="loginStyle.css">
 </head>
 <body>
-<?php
-    require_once("connectUser.php");
-?>
 
 <?php
         if ($_SERVER['REQUEST_METHOD']=="POST"){
              $Username_attempt = $_POST['username'];
              $Password_attempt = $_POST['password'];
 
-            
+          // Checking if the inputs are empty
+          if (empty($username_attempt)) {}
+          elseif (empty($password_attempt)) {}
+          else {
+            // Continue with the password validation
+            require_once("connectUser.php");
 
+            // Querying the database for the information
+            $query = "
+                SELECT hash FROM login WHERE username ='". $username_attempt . "'
+            ";
+            $response = mysqli_query($con, $query);
+          /* needs to be set up stolen from Deanglo
+          // Checking if the username is valid
+          if ($response) {
+            $real_password_hash = mysqli_fetch_assoc($response)["password_hash"];
 
+            if ($real_password_hash == $password_attempt_hash) {
+                $_SESSION["user_logged_in"] = true;
 
-
+                // Special case
+                if ($username_attempt == "Graeme") {
+                    $_SESSION["admin_logged_in"] = true;
+                }
+                
+                header("Location: http://localhost/music_website/index.php");  
+            }
+          */
         }
 
         else{
@@ -37,8 +57,5 @@
 
     <button type="submit">Login</button>
   </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-  </div>
 </form>
+        <?php }?>
