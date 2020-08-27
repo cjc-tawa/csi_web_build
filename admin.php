7856e68c09c4923daf05978d3e439ab368ff82b6
cjc-tawa <67713729+cjc-tawa@users.noreply.github.com>
2020-08-27 12:11:02 +1200

    Got admin working without style
    Now working without style needs to be fixed slightly to become readable

--- admin.php
+++ admin.php
@@ -7,33 +7,19 @@ else{
 }
 ?>
 
-<!doctype html>
-<html lang="en-NZ">
-<head> 
-	<!-- NOTES: 
-		- Link to paste in browser: localhost/CSI_WEB_BUILD/index.php
-	-->
-	<title>PHP Build</title>
-	<meta charset="utf-8">
-	<meta name="author" content="Corey J Christensen">
-	<meta name="description" content="Building a website using HTML, CSS, and PHP.">
-	<meta name="keywords" content="HTML, CSS, PHP, XAMPP, MySQL, website build, databases">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<!-- Fonts imported using Google Fonts. -->
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-	
-	<!-- Stylesheets linked to this website. -->
-	<link rel="stylesheet" href="style.css">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require("connectUser.php"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Cardboard Cutout Design</title>
 </head>
 <body>
-<div id="wrapper">
-        <?php
-        require_once("connectUser.php");
-        ?>
-        
-        <nav id="navbar">
+    <div id="wrapper">
+        <!--<nav id="navbar">
         <div id="bold"><h1>Music Database</h1></div>
                 <nav id='links'>
                         <ul>
@@ -43,62 +29,58 @@ else{
                                 <?php
                                 if (isset($_SESSION["admin_logged_in"])) {
                                 ?>
-                                <li><a href="#">Admin</a></li>
+                                <li><a href="admin.php">Admin</a></li>
                                 <?php
                                 }
                                 ?>
                         </ul>
                 </nav>
-        </nav>
-        <div id='main'>
-        <?php
-        echo $_SESSION['user_logged_in'];
-        echo $_SESSION['admin_logged_in'];
-        ?>
-                                <div id='querybox'>
-                                <table>
-                                <tr>
-                                        <th>UserID</th>
-                                        <th>Username</th>
-                                        <th>Password</th>
-                                </tr>
-                                <?php 
-                                        $query = ("SELECT s.title, a.album, art.artist, g.genre, s.duration, s.size
-                                        FROM songs as s
-                                        JOIN albums as a ON s.album_id = a.album_id
-                                        JOIN artists as art ON s.artist_id = art.artist_id
-                                        JOIN album_to_genre a2g ON a.album_id = a2g.album_id
-                                        JOIN genres g ON a2g.genre_id = g.genre_id
-                                        WHERE 1
-                                        ORDER BY s.title DESC, art.artist DESC");
-                                                        
-                                        $result = mysqli_query($con, $query);
-                                                        
-                                        while($output=mysqli_fetch_array($result))
-                                        {
-                                ?>
-                                
-                                        
-                                                
-                                        <tr>
-                                                <td><?php echo $output['title']; ?></td>
-                                                <td><?php echo $output['album'];?></td>
-                                                <td><?php echo $output['artist']; ?></td>
-                                                <td><?php echo $output['genre']; ?></td>
-                                                <td><?php echo $output['duration']; ?></td>
-                                                <td><?php echo $output['size']; ?></td>
-                                                
-                                        <tr>
-
-                                        
-                                                
-                                        
-                                        <?php
-                                        } 
-                                  ?>
-                                </div>
+        </nav>-->
+        <div id="body">
+            <table id="info-table">
+                <tr>
+                    <th>Users</th>
+                    <th>Password</th>
+                    <th>Actions</th>
+                </tr>
+                <?php 
+                    // Building the query
+                    $query = "
+                        SELECT userID, userName, hash
+                        FROM login
+                        WHERE 1
+                        ORDER BY userName ASC
+                    ";
+                    // Fetching the data
+                    if ($result = mysqli_query($con, $query)) {
+                        // Fetching every row of data
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                <tr>
+                    <td>
+                        <?php echo $row["userName"] ?>
+                    </td>
+                    <td>
+                        <?php echo $row["hash"];?>
+                    </td>
+                    <td> 
+                        <a href="
+                            <?php echo "delete_user.php?user_id=" . $row["user_id"]?>
+                        ">delete</a>
+                        <a href="
+                            <?php echo "new_password.php?user_id=" . $row["user_id"] ?>
+                        ">update</a>
+                    </td>
+                </tr>
+                            <?php
+                        }
+                    }
+                ?>
+            </table>
+            <div id="extra-information">
+                <a href="new_user.php">add user</a>
+            </div>
         </div>
-
-</div>
+    </div>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
-    /*session_start();*/
-    unset($_SESSION["user_logged_in"]);
+    session_start();
     session_destroy();
     header("Location: http://localhost/CSI_WEB_BUILD/index.php");
 ?>
